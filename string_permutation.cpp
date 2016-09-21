692713bf54fc1e95e987cc8ba3d77dedde084769
Nishit Majithia <nishitm@cse.iitk.ac.in>
2016-09-21 21:05:37 +0530

    Recursion

---
+++ string_permutation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+void permutation(string s, int n, int m)
+{
+   if (n == m)
+     cout<<s<<endl;
+   else
+   {
+       for (int i = n; i <= m; i++)
+       {
+          std::swap(s[n], s[i]);
+          permutation(s, n+1, m);
+          std::swap(s[n],s[i]);
+       }
+   }
+}
+
+
+int main()
+{
+  string s;
+  cout<<"Enter string:";
+  cin>>s;
+
+  permutation(s,0,s.length()-1);
+
+  return 0;
+}
